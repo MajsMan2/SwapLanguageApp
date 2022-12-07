30ed2ce8776db3bbc03587c660d2e76037d9248f
Marius Eis <mmklamo299@gmail.com>
2022-12-07 15:27:03 +0100

    igangværendefragments

---
+++ app/src/main/java/com/example/swaplanguageapp/Views/BlogFragment.java
@@ -0,0 +1,21 @@
+package com.example.swaplanguageapp.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.swaplanguageapp.R;
+
+public class BlogFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_blog, container, false);
+    }
+}
