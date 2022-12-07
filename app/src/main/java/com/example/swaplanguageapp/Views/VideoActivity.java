d519f515526cb214f9ad22f1968c360f8fc4a3bf
Marius Eis <mmklamo299@gmail.com>
2022-12-07 14:21:43 +0100

    Sider Oprettet

---
+++ app/src/main/java/com/example/swaplanguageapp/Views/VideoActivity.java
@@ -0,0 +1,16 @@
+package com.example.swaplanguageapp.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.swaplanguageapp.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
