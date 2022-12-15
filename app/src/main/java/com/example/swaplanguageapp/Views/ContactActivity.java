a789a55e1a629f3efdeec57589062d4c8232d3b7
Marius Eis <mmklamo299@gmail.com>
2022-12-15 11:02:05 +0100

    Los Finalos

--- app/src/main/java/com/example/swaplanguageapp/Views/ContactActivity.java
+++ app/src/main/java/com/example/swaplanguageapp/Views/ContactActivity.java
@@ -3,6 +3,8 @@ package com.example.swaplanguageapp.Views;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,5 +19,26 @@ public class ContactActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+    }
+
+    public void goToFcbUrl(View view) {
+        String url = "https://www.facebook.com/swaplanguage/";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void goToLinkdUrl(View view) {
+        String url = "https://www.linkedin.com/company/swap-language/";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void goToInstUrl(View view) {
+        String url = "https://www.instagram.com/swaplanguage/";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
